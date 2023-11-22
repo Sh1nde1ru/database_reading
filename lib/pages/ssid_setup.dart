2f2eafc777f841da79b66ff36ce826e983cdd8e7
Sh1nde1ru <antoninkonig2@gmail.com>
2023-11-22 23:39:10 +0100

    show dialog

--- lib/pages/ssid_setup.dart
+++ lib/pages/ssid_setup.dart
@@ -9,9 +9,75 @@ class SsidSetup extends StatefulWidget {
 
 class _SsidSetupState extends State<SsidSetup> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    String name = "";
+    String password = "";
+    setState(() {
+      name = ssid.text;
+      password = pass.text;
+    });
+  }
+
+  TextEditingController ssid = TextEditingController();
+  TextEditingController pass = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("ssid setup"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 300,
+              child: TextField(
+                controller: ssid,
+                decoration: InputDecoration(
+                    hintText: "SSID",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12))),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            SizedBox(
+              width: 300,
+              child: TextField(
+                controller: pass,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12))),
+              ),
+            ),
+            TextButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Entered Credentials'),
+                      content: Text('SSID: ' +
+                          ssid.text +
+                          "\n" +
+                          "Password: " +
+                          pass.text),
+                      actions: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('ok'))
+                      ],
+                    ),
+                  );
+                },
+                child: Text("Send"))
+          ],
+        ),
+      ),
     );
   }
 }
